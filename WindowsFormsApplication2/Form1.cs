618d70bbc1b1d80b32e73abbd0f4b44efa795678
MaryO <MaryO@MARYPC>
2016-10-03 22:10:32 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class MainForm : Form
+    {
+        public delegate void Helper();
+        public Helper HelperDelegate1, HelperDelegate2, HelperDelegate3;
+        MyGlass glass1, glass2, glass3;
+
+       
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            glass1 = new MyGlass(1);
+            glass2 = new MyGlass(2);
+            glass3 = new MyGlass(3);
+
+            glass1.Size = new Size(150, 180);
+            glass2.Size = new Size(150, 180);
+            glass3.Size = new Size(150, 180);
+
+            glass1.SizeMode = PictureBoxSizeMode.StretchImage;
+            glass2.SizeMode = PictureBoxSizeMode.StretchImage;
+            glass3.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            glass1.Location = new Point(90, 50);
+            glass2.Location = new Point(190, 50);
+            glass3.Location = new Point(290, 50);
+            
+
+            glass1.BackColor = Color.Transparent;
+            glass2.BackColor = Color.Transparent;
+            glass3.BackColor = Color.Transparent;
+
+            Random rnd = new Random();
+            int num = rnd.Next(1, 4);
+            if (glass1.Id == num) glass1.IsHasBall = true;
+            if (glass2.Id == num) glass2.IsHasBall = true;
+            if (glass3.Id == num) glass3.IsHasBall = true;
+            glass1.OpenGlass();
+            glass2.OpenGlass();
+            glass3.OpenGlass();
+            Controls.Add(glass1);
+            Controls.Add(glass2);
+            Controls.Add(glass3);
+        }
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            glass1.CloseGlass();
+            glass2.CloseGlass();
+            glass3.CloseGlass();
+        }
+    }
+}
---
+++ WindowsFormsApplication2/MyGlass.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    class MyGlass:PictureBox
+    {
+        bool isHasBall;
+        Image closedImg, withBallImg, emptyImg;
+        int id;
+        public bool IsHasBall
+        {
+            get
+            {
+                return isHasBall;
+            }
+
+            set
+            {
+                isHasBall = value;
+            }
+        }
+
+        public Image ClosedImg
+        {
+            get
+            {
+                return closedImg;
+            }
+
+            set
+            {
+                closedImg = value;
+            }
+        }
+
+        public Image WithBallImg
+        {
+            get
+            {
+                return withBallImg;
+            }
+
+            set
+            {
+                withBallImg = value;
+            }
+        }
+
+        public Image EmptyImg
+        {
+            get
+            {
+                return emptyImg;
+            }
+
+            set
+            {
+                emptyImg = value;
+            }
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            private set
+            {
+                id = value;
+            }
+        }
+
+        public MyGlass(int id)
+        {
+            IsHasBall = false;
+            ClosedImg = Properties.Resources.Close;
+            WithBallImg = Properties.Resources.OpenedFull;
+            EmptyImg = Properties.Resources.OpenedEmpty;
+            Id = id;
+            Image = ClosedImg;
+            Click += GlassClick;
+        }
+        public void GlassClick(object sender, EventArgs e)
+        {
+            if (Image == ClosedImg)
+            {
+                OpenGlass();
+            }
+            else
+            {
+                CloseGlass();
+            }
+        }
+        public void OpenGlass()
+        {
+            if (IsHasBall) Image = WithBallImg;
+            else Image = EmptyImg;
+            Size = new Size(100, 100);
+        }
+
+
+        public void CloseGlass()
+        {
+            Image = ClosedImg;
+            Size = new Size(80, 100);
+        }
+    }
+}
