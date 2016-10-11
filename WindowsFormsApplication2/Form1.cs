2111b7b40247b1649c2f08a3e27ed5bef4ee2769
Ekaterina-kuzmina <ekaterina-kuzmina95@yandex.ru>
2016-10-11 17:57:05 +0300

    назвала переменную rnd (так логичнее)

--- WindowsFormsApplication2/Form1.cs
+++ WindowsFormsApplication2/Form1.cs
@@ -43,8 +43,8 @@ namespace WindowsFormsApplication2
             glass2.BackColor = Color.Transparent;
             glass3.BackColor = Color.Transparent;
 
-            Random rnd2 = new Random(); // изменила название переменной
-            int num = rnd2.Next(1, 4);
+            Random rnd = new Random(); // изменила название переменной
+            int num = rnd.Next(1, 4);
             if (glass1.Id == num) glass1.IsHasBall = true;
             if (glass2.Id == num) glass2.IsHasBall = true;
             if (glass3.Id == num) glass3.IsHasBall = true;
